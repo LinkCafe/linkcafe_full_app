b38bb6636f4dadfc4c5dd1f79f7a3cccbf759e69
LeomarEs19 <lespanacastillo20@gmail.com>
2024-02-04 18:02:35 -0500

    Ruta y Controlador De Articulos

---
+++ backend/src/routes/articulos.routes.js
@@ -0,0 +1,10 @@
+import { Routes } from "express";
+import { createArticles, deleteArticles, showAArticles, showArticles, updateArticles } from "../controllers/articulos.controllers";
+
+const router = Routes()
+
+router.get("/articulos", showArticles)
+router.post("/articulos", createArticles)
+router.put("/articulos", updateArticles)
+router.get("/articulos", showAArticles)
+router.delete("/articulos", deleteArticles)
