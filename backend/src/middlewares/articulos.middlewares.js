a6ded8121b5613dc60b5acd0c81615e683659e6c
LeomarEs19 <lespanacastillo20@gmail.com>
2024-02-28 13:05:34 -0500

    Implementacion middlewares En articulos

---
+++ backend/src/middlewares/articulos.middlewares.js
@@ -0,0 +1,13 @@
+import { check } from "express-validator";
+
+export const middlewaresShowArticles = [
+    check('nombre','nombre, Campo obligatorio').not().isEmpty(),
+    check('enlace','enlace Obligatorio').not().isEmpty(),
+    check('autor','autor obligatorio').not().isEmpty(),
+];
+
+export const middlewaresUpdateArticles = [
+    check('nombre','nombre, Necesario').not().isEmpty(),
+    check('enlace','enlace Necesario').optional(),
+    check('autor','autor Obligatorio').not().isEmpty(),
+]
