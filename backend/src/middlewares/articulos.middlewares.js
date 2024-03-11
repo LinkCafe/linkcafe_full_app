6c84a56785463fb86f05d67fe29c7e6c5b000e29
LeomarEs19 <lespanacastillo20@gmail.com>
2024-03-10 19:00:37 -0500

    Modificacion En Articulos Documentacion y Middlewares

--- backend/src/middlewares/articulos.middlewares.js
+++ backend/src/middlewares/articulos.middlewares.js
@@ -1,13 +1,13 @@
 import { check } from "express-validator";
 
 export const middlewaresShowArticles = [
-    check('nombre','nombre, Campo obligatorio').not().isEmpty().isString(),
-    check('enlace','enlace Obligatorio').not().isEmpty(),
-    check('autor','autor obligatorio').not().isEmpty(),
+    check('nombre','nombre, Campo obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }).isString(),
+    check('enlace','enlace Obligatorio').not().isEmpty().isLength({ max: 225 }),
+    check('autor','autor obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }),
 ];
 
 export const middlewaresUpdateArticles = [
-    check('nombre','nombre, Necesario').not().isEmpty().isString,
-    check('enlace','enlace Necesario').optional(),
-    check('autor','autor Obligatorio').not().isEmpty(),
+    check('nombre','nombre, Necesario').not().isEmpty().isLength({ min: 3, max: 100 }).isString(),
+    check('enlace','enlace Necesario').optional().isLength({min: 3, max: 225 }),
+    check('autor','autor Obligatorio').not().isEmpty().isLength({ min:3, max: 100 }),
 ]
