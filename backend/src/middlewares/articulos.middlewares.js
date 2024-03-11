4140705d7eb7c6904bb711a9786f3720c3a3e473
LeomarEs19 <lespanacastillo20@gmail.com>
2024-03-10 19:05:11 -0500

    Correccion En Middlewares Articulos

--- backend/src/middlewares/articulos.middlewares.js
+++ backend/src/middlewares/articulos.middlewares.js
@@ -1,13 +1,13 @@
 import { check } from "express-validator";
 
 export const middlewaresShowArticles = [
-    check('nombre','nombre, Campo obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }).isString(),
-    check('enlace','enlace Obligatorio').not().isEmpty().isLength({ max: 225 }),
-    check('autor','autor obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }),
+    check('nombre','nombre, Campo obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }).isString().optional(),
+    check('enlace','enlace Obligatorio').not().isEmpty().isLength({ max: 225 }).optional(),
+    check('autor','autor obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }).optional(),
 ];
 
 export const middlewaresUpdateArticles = [
-    check('nombre','nombre, Necesario').not().isEmpty().isLength({ min: 3, max: 100 }).isString(),
-    check('enlace','enlace Necesario').optional().isLength({min: 3, max: 225 }),
-    check('autor','autor Obligatorio').not().isEmpty().isLength({ min:3, max: 100 }),
+    check('nombre','nombre, Necesario').not().isEmpty().isLength({ min: 3, max: 100 }).isString().optional(),
+    check('enlace','enlace Necesario').optional().isLength({min: 3, max: 225 }).optional(),
+    check('autor','autor Obligatorio').not().isEmpty().isLength({ min:3, max: 100 }).optional().optional(),
 ]
