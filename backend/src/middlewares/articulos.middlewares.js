145dc42689fd6de630a148917d76d7c00825b88a
LeomarEs19 <lespanacastillo20@gmail.com>
2024-03-10 19:12:29 -0500

    Cambios En middlewares Articulos

--- backend/src/middlewares/articulos.middlewares.js
+++ backend/src/middlewares/articulos.middlewares.js
@@ -1,9 +1,9 @@
 import { check } from "express-validator";
 
 export const middlewaresShowArticles = [
-    check('nombre','nombre, Campo obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }).isString().optional(),
-    check('enlace','enlace Obligatorio').not().isEmpty().isLength({ max: 225 }).optional(),
-    check('autor','autor obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }).optional(),
+    check('nombre','nombre, Campo obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }).isString(),
+    check('enlace','enlace Obligatorio').not().isEmpty().isLength({ max: 225 }),
+    check('autor','autor obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }),
 ];
 
 export const middlewaresUpdateArticles = [
