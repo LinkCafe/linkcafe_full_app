0df9c5953f77382a9a125ab9bdfbfd130ec8380b
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2024-03-13 20:18:55 -0500

    todo bien

--- backend/src/middlewares/usuarios.middlewares.js
+++ backend/src/middlewares/usuarios.middlewares.js
@@ -1,12 +1,12 @@
 import {check} from 'express-validator'
 
 export const middlewaresCreateUsers =[
-    check('nombre_completo','nombre, campo oblogatorio').not().isEmpty(),
-    check('correo','correo invalido').isEmail().normalizeEmail(),
-    check('clave', 'la clave es de caracter obligatorio').not().isEmpty(),
+    check('nombre_completo','nombre, campo oblogatorio').not().isEmpty().isLength({ min: 3, max: 100 }).isString(),
+    check('correo','correo invalido').not().isEmail().normalizeEmail(),
+    check('clave', 'la clave es de caracter obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }),
 ];
 export const middlewaresUpdate=[
-  check('nombre_completo','falta el nombre').optional(),
+  check('nombre_completo','falta el nombre').optional().isLength({ min: 3, max: 100 }).isString(),
   check('correo','correo invalido').optional().isEmail().normalizeEmail(),
-  check('clave','clave').optional(),
+  check('clave','clave').optional().isLength({ min: 3, max: 100 }).isString(),
 ];
