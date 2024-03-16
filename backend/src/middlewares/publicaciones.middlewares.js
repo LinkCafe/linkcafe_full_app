d4ccdec0effb9bb3d725ff870ec7ab41fd748e3b
Alejo6648 <alorgomez2006@gmail.com>
2024-03-16 16:27:54 -0500

    Reconfiguracion de la documentacion de publicaciones y añadiendo nuevos parametros al middleware de publicaciones

--- backend/src/middlewares/publicaciones.middlewares.js
+++ backend/src/middlewares/publicaciones.middlewares.js
@@ -1,16 +1,16 @@
 import { check } from 'express-validator';
 
 export const middlewaresCreatePublics = [
-    check('nombre', 'nombre, campo obligatorio').not().isEmpty().isLength({ min: 3, max: 50 }),
+    check('nombre', 'nombre, campo obligatorio').not().isEmpty().isLength({ min: 2, max: 50 }),
     check('descripcion', 'Ingrese alguna descripcion').not().isEmpty(),
     check('imagen', 'Imagen opcional').optional().isLength({ max: 255 }),
     check('fuentes', 'Ingrese alguna fuente suministrada').not().isEmpty().isLength({ max: 100 }),
     check('tipo', 'Obligatorio registrar un tipo').not().isEmpty(),
-    check('id_usuario', 'Id usuario obligatorio').not().isEmpty().isInt()
+    check('id_usuario', 'Id usuario obligatorio').not().isEmpty().isInt({ max: 11 })
 ];
 
 export const middlewaresUpdatePublics = [
-    check('nombre', 'Nombre invalido').optional().isLength({ min: 3, max: 50 }),
+    check('nombre', 'Nombre invalido').optional().isLength({ min: 2, max: 50 }),
     check('descripcion', 'Descripcion invalida').optional(),
     check('imagen', 'Imagen opcional').optional().isLength({ max: 255 }),
     check('fuentes', 'Fuente invalida').optional().isLength({ max: 100 }),
