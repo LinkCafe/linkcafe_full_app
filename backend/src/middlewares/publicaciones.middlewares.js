9a464d24f751e07af027c63b7a5fd0b9790d128f
Alejo6648 <alorgomez2006@gmail.com>
2024-03-16 16:14:55 -0500

    Reconfigurando la base de documentacion de publicaciones y añadiendo parametros al middleware de publicaciones

--- backend/src/middlewares/publicaciones.middlewares.js
+++ backend/src/middlewares/publicaciones.middlewares.js
@@ -1,18 +1,19 @@
-import {check} from 'express-validator'
+import { check } from 'express-validator';
 
-export const middlewaresCreatePublics =[
-    check('nombre','nombre, campo obligatorio').not().isEmpty(),
-    check('descripcion','Ingrese alguna descripcion').not() .isEmpty(),
-    check('imagen', 'Imagen opcional').optional(),
-    check('fuentes', 'Ingrese alguna fuente suministrada').not().isEmpty(),
-    check('tipo', 'Obligatorio registrar un tipo').not() .isEmpty(),
-    check('id_usuario','Id usuario obligatorio').not() .isEmpty().isInt()
+export const middlewaresCreatePublics = [
+    check('nombre', 'nombre, campo obligatorio').not().isEmpty().isLength({ min: 3, max: 50 }),
+    check('descripcion', 'Ingrese alguna descripcion').not().isEmpty(),
+    check('imagen', 'Imagen opcional').optional().isLength({ max: 255 }),
+    check('fuentes', 'Ingrese alguna fuente suministrada').not().isEmpty().isLength({ max: 100 }),
+    check('tipo', 'Obligatorio registrar un tipo').not().isEmpty(),
+    check('id_usuario', 'Id usuario obligatorio').not().isEmpty().isInt()
 ];
-export const middlewaresUpdatePublics=[
-    check('nombre','Nombre invalido').optional(),
-    check('descripcion','Descripcion invalida').optional(),
-    check('imagen', 'Imagen opcional').optional(),
-    check('fuentes', 'Fuente invalida').optional(),
+
+export const middlewaresUpdatePublics = [
+    check('nombre', 'Nombre invalido').optional().isLength({ min: 3, max: 50 }),
+    check('descripcion', 'Descripcion invalida').optional(),
+    check('imagen', 'Imagen opcional').optional().isLength({ max: 255 }),
+    check('fuentes', 'Fuente invalida').optional().isLength({ max: 100 }),
     check('tipo', 'Tipo invalido').optional(),
-    check('id_usuario','Id usuario invalido').optional().isInt()
+    check('id_usuario', 'Id usuario invalido').optional().isInt()
 ];
