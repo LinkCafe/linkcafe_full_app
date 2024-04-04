4fdb2d7470a8561304b306e9e4cdce9d58b9d303
Diego Duran <96787803+fxckcode@users.noreply.github.com>
2024-04-04 13:18:40 -0500

    Update usuarios.middlewares.js

--- backend/src/middlewares/usuarios.middlewares.js
+++ backend/src/middlewares/usuarios.middlewares.js
@@ -2,11 +2,11 @@ import {check} from 'express-validator'
 
 export const middlewaresCreateUsers =[
     check('nombre_completo','nombre, campo oblogatorio').not().isEmpty().isLength({ min: 3, max: 100 }).isString(),
-    check('correo','correo invalido').not().isEmail().normalizeEmail(),
+    check('correo','correo invalido').not().isEmpty().isEmail(),
     check('clave', 'la clave es de caracter obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }),
 ];
 export const middlewaresUpdate=[
   check('nombre_completo','falta el nombre').optional().isLength({ min: 3, max: 100 }).isString().not().isEmpty(),
-  check('correo','correo invalido').optional().isEmail().normalizeEmail().not().isEmpty(),
+  check('correo','correo invalido').optional().isEmail().not().isEmpty(),
   check('clave','clave').optional().isLength({ min: 3, max: 100 }).isString().not().isEmpty(),
 ];
