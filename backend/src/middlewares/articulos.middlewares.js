46e8c2dbc309e621d342d20d2f1f01cb0a101d0f
Diego Duran <96787803+fxckcode@users.noreply.github.com>
2024-04-04 13:19:01 -0500

    Update articulos.middlewares.js

--- backend/src/middlewares/articulos.middlewares.js
+++ backend/src/middlewares/articulos.middlewares.js
@@ -4,10 +4,12 @@ export const middlewaresShowArticles = [
     check('nombre','nombre, Campo obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }).isString(),
     check('enlace','enlace Obligatorio').not().isEmpty().isLength({ max: 225 }),
     check('autor','autor obligatorio').not().isEmpty().isLength({ min: 3, max: 100 }),
+    check('id_usuario','Usuario oblicagatorio').not().isEmpty().isInt()
 ];
 
 export const middlewaresUpdateArticles = [
     check('nombre','nombre, Necesario').not().isEmpty().isLength({ min: 3, max: 100 }).isString().optional(),
     check('enlace','enlace Necesario').not().isEmpty().isLength({min: 3, max: 225 }).isString().optional(),
-    check('autor','autor Obligatorio').not().isEmpty().isLength({ min:3, max: 100 }).isString().optional()
+    check('autor','autor Obligatorio').not().isEmpty().isLength({ min:3, max: 100 }).isString().optional(),
+    check('id_usuario','Usuario oblicagatorio').not().isEmpty().isInt().optional()
 ]
